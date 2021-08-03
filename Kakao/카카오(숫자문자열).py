db77a289d3162a3838117db283ce681cb26a3a54
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2021-08-03 18:03:54 +0900

    카카오(숫자문자열)

---
+++ Kakao/카카오(숫자문자열).py
@@ -0,0 +1,20 @@
+
+def solution(s):
+    word_dict={"zero":"0",
+                "one":"1",
+                "two":"2",
+                "three":"3",
+                "four":"4",
+                "five":"5",
+                "six":"6",
+                "seven":"7",
+                "eight":"8",
+                "nine":"9"
+                }
+
+    for key in word_dict.keys():
+        if key in s:
+            s=s.replace(key,word_dict[key])
+    s=int(s)
+    return s
+
